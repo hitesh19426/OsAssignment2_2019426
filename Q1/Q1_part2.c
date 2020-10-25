8a83e514d3f784b2f5dc2217a78196d9acdb0ca3
hitesh19426 <55842142+hitesh19426@users.noreply.github.com>
2020-10-26 00:48:05 +0530

    Update Q1_part2.c

--- Q1/Q1_part2.c
+++ Q1/Q1_part2.c
@@ -1,2 +1,32 @@
-/* Name: Your Name
-   Roll_Number: Your Roll_Number */
+/* Name: Hitesh Garg
+   Roll_Number: 2019426 */
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<unistd.h>
+#include<pthread.h>
+void *increament(void *vargp);
+int varp=10;
+
+int main(int argc, char const *argv[])
+{
+	/* code */
+	pthread_t tid;
+	pthread_create(&tid, NULL, increament, NULL);
+
+	printf("Parent thread. ");
+	for (; varp<=100;varp++)
+		printf("%d\n", varp);
+
+	pthread_join(tid, NULL);
+	return 0;
+}
+
+void *increament(void *vargp)
+{
+	printf("%s\n", "Child thread");
+	for (;varp>=-90;varp--)
+	{
+		printf("%d\n",varp );
+	}
+}
