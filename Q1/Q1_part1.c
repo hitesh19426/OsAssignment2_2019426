3deee384781af6a151b0a13f93ad3e999de3c6f7
hitesh19426 <55842142+hitesh19426@users.noreply.github.com>
2020-10-28 23:03:39 +0530

    Update Q1_part1.c

--- Q1/Q1_part1.c
+++ Q1/Q1_part1.c
@@ -1,6 +1,5 @@
 /* Name: Hitesh Garg 
-   Roll_Number: Y2019426 */
-
+   Roll_Number: 2019426 */
 #include<stdio.h>
 #include<stdlib.h>
 #include<unistd.h>
@@ -16,22 +15,18 @@ int main(int argc, char const *argv[])
 	pid_t pid=fork();
 	if (pid<0)
 	{
-		//printf("%s\n", "Process creation failed");
 		exit(1);
 	}
 	else if (pid==0)
 	{
-		//printf("child process successfully created!\n");
 		childprocess();
-		//exit(1);
 	}
 	else
 	{
-		//wait(NULL);
-		//printf("Parent process successfully created!\n");
 		parentprocess();
 		wait(NULL);
 	}
+
 	return 0;
 }
 
@@ -40,7 +35,7 @@ void childprocess()
 	//varp=10;
 	while (varp!=-91)
 	{
-		printf("%d \n",varp--);
+		printf("Child %d \n",varp--);
 	}
 }
 
@@ -49,6 +44,6 @@ void parentprocess()
 	// varp=10;
 	while (varp!=101)
 	{
-		printf("%d \n",varp++);
+		printf("Parent %d \n",varp++);
 	}
 }
